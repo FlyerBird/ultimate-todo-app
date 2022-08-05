2ad3a48a923ab98ab632ba0804998b8ad8fa22e3
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-08-05 21:20:45 +0200

    initialazing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import './App.css';
 import tasks from './list.json';
+import SearchBar from './components/SearchBar';
+import AllTasks from './components/AllTasks';
+import CreateTask from './components/CreateTask';
+import UrgentTasks from './components/UrgentTasks';
 
 function App() {
   return (
     <div className="App">
+    <h1>MY TO DO LIST</h1>
 
+    <div className='Header'>
+      <div>
+          <CreateTask />
+        </div>
+        <div>
+          <UrgentTasks />
+        </div>
+    </div>
+   
+        <div>
+          <SearchBar />
+        </div>
+        <div>
+          <AllTasks />
+        </div>
+        
     </div>
   );
 }
---
+++ src/components/UrgentTasks.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UrgentTasks() {
+  return (
+    <div>UrgentTasks</div>
+  )
+}
